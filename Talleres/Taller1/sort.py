164854e7d193ffc78f557b4593d27c0a6a18bdb2
Alejandro Morales <a.moralesc@javeriana.edu.co>
2022-07-24 19:26:46 -0500

    Remove unused import

--- Talleres/Taller1/sort.py
+++ Talleres/Taller1/sort.py
@@ -1,26 +1,25 @@
-from re import I
-
-
 def naive_bubble_sort(seq):
     for i in range(len(seq)):
-        for j in range(len(seq)-1):
-            if seq[j+1] < seq[j]:
-                seq[j], seq[j+1] = seq[j+1], seq[j]
+        for j in range(len(seq) - 1):
+            if seq[j + 1] < seq[j]:
+                seq[j], seq[j + 1] = seq[j + 1], seq[j]
     return seq
+
 
 def optimized_bubble_sort(seq):
     for i in range(len(seq)):
-        for j in range(len(seq)-1-i):
-            if seq[j+1] < seq[j]:
-                seq[j], seq[j+1] = seq[j+1], seq[j]
+        for j in range(len(seq) - 1 - i):
+            if seq[j + 1] < seq[j]:
+                seq[j], seq[j + 1] = seq[j + 1], seq[j]
     return seq
+
 
 def insertion_sort(seq):
     for j in range(1, len(seq)):
         k = seq[j]
         i = j - 1
         while 0 < i and k < seq[i]:
-            seq[i+1] = seq[i]
+            seq[i + 1] = seq[i]
             i -= 1
-        seq[i+1] = k
+        seq[i + 1] = k
     return seq
